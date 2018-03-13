9d7b4db1308331e2887f1a45204e9da7377fc2ce
Mazzatadavide <davide.mazzata@gmail.com>
2018-03-13 14:52:57 +0100

    Inverted X_MAX logic

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -784,7 +784,7 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -21
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS 350
